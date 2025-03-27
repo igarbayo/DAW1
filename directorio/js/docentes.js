114621eaed5859b638028eb1b25c014294ae9586
igarbayo <ignacio.garbayo@rai.usc.es>
2025-03-27 16:38:20 +0100

    Lista de Efectos

--- directorio/js/docentes.js
+++ directorio/js/docentes.js
@@ -7,3 +7,49 @@ function mostrarSeccion(id) {
     // Mostrar la sección seleccionada
     document.getElementById(id).style.display = 'block';
 }
+
+document.getElementsByClassName("ciencias-link")[0].addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    mostrarSeccion("ciencias");
+});
+
+const enlaces = document.getElementsByTagName("a");
+
+for (let enlace of enlaces) {
+    if (enlace.textContent.trim() === "Idiomas") {
+        enlace.addEventListener("click", function(event) {
+            event.preventDefault(); // Evita que el enlace recargue la página
+            mostrarSeccion("idiomas");
+        });
+        break; // Salimos del bucle una vez que encontramos el enlace correcto
+    }
+}
+
+const enlacehum = document.getElementsByName("humanidades-link");
+
+if (enlacehum.length > 0) {
+    enlacehum[0].addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que el enlace recargue la página
+        mostrarSeccion("humanidades");
+    });
+}
+
+const links = document.links;
+
+for (let link of links) {
+    if (link.textContent.trim() === "Tecnología") {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Evita que el enlace recargue la página
+            mostrarSeccion("tecnologia");
+        });
+        break; // Salimos del bucle una vez que encontramos el enlace correcto
+    }
+}
+
+document.querySelector(".artes-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el enlace recargue la página
+    mostrarSeccion("artes");
+});
+
+
+
